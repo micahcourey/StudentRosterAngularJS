5fa3febfbb56f3b6f46977fe65ad6e7a4a6a85e2
micahcourey <micahjcourey@gmail.com>
2015-09-20 22:18:03 -0700

    add project files and test data

---
+++ js/StudentsController.js
@@ -0,0 +1,8 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    { name: "Micah Courey" },
+    { name: "Jessica Martin" },
+    { name: "Sandy Smith" },
+    { name: "Ryan Samuels" }
+  ]
+})
